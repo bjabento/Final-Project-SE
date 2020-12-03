9bda017b740bdb065799c22f532180f0293dcbbb
Bernardo Bento <64841055+bjabento@users.noreply.github.com>
2020-12-03 11:49:28 +0000

    Código do Sistema de Rega
    !Código por testar!
    Alterar posição ideal (Servo Motor)

---
+++ sistema_de_rega.ino
@@ -0,0 +1,48 @@
+#include <DHT.h>
+#define DHTTYPE DHT11
+#define DHTPIN 7  
+DHT dht(DHTPIN, DHTTYPE);
+Servo s1;
+
+int map1;
+int n = 30;
+int pos = 0;
+float hum, temp;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+  pinMode(10, OUTPUT);
+  dht.begin();
+  s1.attach (10);
+
+}
+
+void loop() {
+  //Servo
+  s1.write(pos);
+  //Leitura Humidade
+ hum = dht.readHumidity();
+ //Leitura temperatura
+ temp = dht.readTemperature();
+  //Verifica se houve erros
+  if (isnan(hu) || isnan(te)) {
+    Serial.println(F("Falha ao ler o sensor"));
+    return;
+  }
+  else {
+    Serial.print("Humidade: ");
+    Serial.print(hum);
+    Serial.print(" %t");
+    Serial.print("Temperatura: ");
+    Serial.print(temp);
+    Serial.println(" ºC");
+    while ( hum < 20)
+    {
+      //Verificar melhor posi??o - POR FAZER
+      pos = 90;
+      s1.write(pos);
+    }
+    }
+  }
+}
